fdfd724778383a4aebc67541fdf6614ac6a15f86
kapunska <berktnc@gmail.com>
2017-06-18 01:56:38 +0300

    no message

--- sandbox.py
+++ sandbox.py
@@ -6,25 +6,24 @@ width = int(input("enter the sandbox width: "))
 Matrix = [[0 for i in range(width)] for j in range(height)]
 
 #gets the matrix inputs from the user.
-for i in range(0, height):
-     for j in range(0, width):
+for i in range(height):
+     for j in range(width):
           print ("Enter the elemnt at %d, %d: " %(i , j))
           Matrix[i][j]= int(input())
 
-##ctr = 0
-##for i in range(0, height):
-##     for j in range(0, width):
-##          if Matrix[i][j] > 3:
-##               Matrix[i][j] -= 4
-##               for k in range (4):
-##                    if i-1 > 0:
-##                         Matrix[i-1][j] += 1
-##                    if j-1 > 0:
-##                         Matrix[i][j-1] += 1
-##                    if j+1 < width:
-##                         Matrix[i][j+1] += 1
-##                    if i+1 < height:
-##                         Matrix[i+1][j] += 1
+ctr = 0
+for i in range(0, height):
+     for j in range(0, width):
+          if Matrix[i][j] > 3:
+               Matrix[i][j] -= 4
+               if i-1 > 0:
+                    Matrix[i-1][j] += 1
+               if j-1 > 0:
+                    Matrix[i][j-1] += 1
+               if j+1 < width:
+                    Matrix[i][j+1] += 1
+               if i+1 < height:
+                    Matrix[i+1][j] += 1
                     
 
                  
